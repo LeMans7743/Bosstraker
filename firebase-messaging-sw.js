ace7e912b38e55184d8a4d616f9ce6c320a31355
LeMans7743 <leman7743@gmail.com>
2025-09-05 00:21:40 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,25 @@
-importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging.js");
+// 必須在 GitHub Pages 根目錄
+importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging-compat.js");
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyC6mq6ukjzpNmaIy5dLewwHqrJTTpaB2jA",
   authDomain: "bosstrackerweb.firebaseapp.com",
   projectId: "bosstrackerweb",
   storageBucket: "bosstrackerweb.firebasestorage.app",
   messagingSenderId: "620047423570",
-  appId: "1:620047423570:web:2d1881a9edc1a1b6cba78b",
-  measurementId: "G-9Y5MM0C4Y8"
-});
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  appId: "1:620047423570:web:2d1881a9edc1a1b6cba78b"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
+// 背景推播通知處理
 messaging.onBackgroundMessage((payload) => {
+  console.log("收到背景訊息: ", payload);
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
+    icon: "/icon.png" // 你可以自訂一張小圖
   });
 });
