03cc8a590ada80edc0f0a4e44ca785c4c0ad6353
LeMans7743 <leman7743@gmail.com>
2025-09-05 00:07:14 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
-importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging.js");
 
-// TODO: 換成你的 firebaseConfig
 firebase.initializeApp({
   apiKey: "AIzaSyC6mq6ukjzpNmaIy5dLewwHqrJTTpaB2jA",
   authDomain: "bosstrackerweb.firebaseapp.com",
@@ -11,15 +10,13 @@ firebase.initializeApp({
   appId: "1:620047423570:web:2d1881a9edc1a1b6cba78b",
   measurementId: "G-9Y5MM0C4Y8"
 });
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("[SW] 背景收到:", payload);
-  const title = payload.notification?.title || "BOSS 通知";
-  const options = {
-    body: payload.notification?.body || "",
-    icon: "/favicon.ico"
-  };
-  self.registration.showNotification(title, options);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+  });
 });
